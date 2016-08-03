abd62fed9efdeacba8e20032d2a226ffa9bb3239
Brad Westfall <brad@azpixels.com>
2016-08-03 08:26:08 -0700

    remove webpack, add examples

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,23 @@
-import { Is } from '../index'
+import { Is, Schema, errorFormat } from '../lib'
 
-Is.numeric()
+// Base Rules
+let id = Is.numeric().positive().integer().required()
+let title = Is.string().length(40).required()
+
+// Schema Inventory
+const schema = new Schema({
+  email: Is.string().email().required(),
+  password: Is.string().ascii().minLength(8).maxLength(100),
+  userId: id.label('User ID'),
+  userTypeId: Is.numeric().in([1, 2, 3]).required()
+})
+
+
+let loginFormSchema = schema.clone(['email', 'password'])
+
+let errors = loginFormSchema.validate({
+  email: 'test@test.com',
+  password: 'somepassword'
+})
+
+console.log(errors) // outputs {}
